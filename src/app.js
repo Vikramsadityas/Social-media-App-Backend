a765e6f387fde818dbf718fdbed151d2378c5c52
Vikram13shri <vikramsadityas@gmail.com>
2024-03-12 21:08:06 +0530

    this is our util folder

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app=express()
+app.use(cors({
+    origin:process.env.CORS_URL,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded( { extended: true } ))
+app.use(express.static("public"))
+// parse cookies    
+app.use(cookieParser())
+
+export {app}
---
+++ src/utils/apiresponse.js
@@ -0,0 +1,12 @@
+class handleresponse{
+    constructor(
+        statuscode,
+        data,
+        message="Success",
+    ){
+        this.statuscode=statuscode;
+        this.data=data;
+        this.message=message;
+        this.success=statuscode<400;
+    }
+}
